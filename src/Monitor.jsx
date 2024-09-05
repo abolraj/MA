f858c41e1ff0b7c3fc19c2ce203e65477cd27012
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-05 09:49:53 +0330

    feat - Add Monitor component

---
+++ src/Monitor.jsx
@@ -0,0 +1,11 @@
+function Monitor({text}) {
+    return (
+        <div className="monitor bg-base-100 bordered border-2 border-info rounded-md p-2 w-72 h-10">
+            <p>
+                {text}
+            </p>
+        </div>
+    )
+}
+
+export default Monitor
