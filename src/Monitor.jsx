a4d3d64618bcfb96f51a7d1427fb17b5abf42769
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-05 13:25:17 +0330

    feat - Beaty txt color

--- src/Monitor.jsx
+++ src/Monitor.jsx
@@ -1,6 +1,6 @@
 function Monitor({text}) {
     return (
-        <div className="monitor text-lg flex items-center justify-start text-left bg-base-100 bordered border-2 border-info rounded-md p-2 px-4 h-10">
+        <div className="monitor text-lg flex items-center justify-start text-left bg-base-100 bordered border-2 border-info rounded-md p-2 text-cyan-400 px-4 h-10">
             <p>
                 {text}
             </p>
