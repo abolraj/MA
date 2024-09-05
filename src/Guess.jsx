99302c419dbcdc516aac949c4d253254fa720a1e
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-05 12:03:06 +0330

    fix - Fix the play phases

--- src/Guess.jsx
+++ src/Guess.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Letter from "./Letter";
 import Monitor from "./Monitor";
-
+import $ from 'jquery'
 
 function Guess({ words }) {
 
@@ -9,6 +9,7 @@ function Guess({ words }) {
     const [wordIndex, setWordIndex] = useState(0)
     const [banner, setBanner] = useState()
     const [isWinner, setIsWinner] = useState(null)
+    const [playCount, setPlayCount] = useState(0)
     const [word, setWord] = useState(words[wordIndex])
     const [letters, setLetters] = useState(word.split('').sort(() => .5 - Math.random()))
 
@@ -17,10 +18,10 @@ function Guess({ words }) {
     const onLetterChoose = function (letter) {
         setMonitorWord(t => t + letter)
         // debugger
-        if(monitorWord.length+1 >= word.length){
-            if(monitorWord+letter == word){
+        if (monitorWord.length + 1 >= word.length) {
+            if (monitorWord + letter == word) {
                 onWin()
-            }else{
+            } else {
                 onLose()
             }
         }
@@ -30,38 +31,52 @@ function Guess({ words }) {
         alert('ended !')
     }
 
-    const alrt = function(text){
+    const alrt = function (text) {
         setBanner(text)
-        setTimeout(()=>setBanner(null),4000)
+        setTimeout(() => {
+            setBanner(null)
+        }, 4000)
     }
-
     const onWin = function () {
         alrt('Perfect ! 🎉🎊')
         setIsWinner(true)
-
-        debugger
-        if(wordIndex >= words.length-1){
+        setPlayCount(c=>c+1)
+        if (wordIndex >= words.length - 1) {
             onEnd()
-        }else{
-            setWordIndex(i=>i+1)
+        } else {
+            setWordIndex(i => i + 1)
         }
     }
 
     const onLose = function () {
+        // debugger
         setIsWinner(false)
+        setPlayCount(c=>c+1)
         alrt('Try Again ! 😕')
 
     }
 
+    const reset = function () {
+        const word = words[wordIndex]
+        console.log(isWinner, '|new word:', word)
+        setMonitorWord('')
+        setWord(()=>word)
+        setLetters(()=>word.split('').sort(() => .5 - Math.random()))
+    }
+
+    useEffect(() => {
+        reset()
+    }, [playCount])
+
     return (
-        <div className="flex flex-col gap-2 mt-10">
+        <div className="container-sm w-72 max-w-72 flex flex-col gap-2 mt-10">
             {banner &&
                 <h2 className={"text-2xl" + (isWinner ? " text-success " : " text-error ")}>{banner}</h2>
             }
             <Monitor text={monitorWord} />
-            <div className="letters flex flex-wrap gap-2 justify-between">
+            <div className="letters flex flex-wrap gap-2 justify-center">
                 {letters.map((letter, i) =>
-                    <Letter letter={letter} onChoose={onLetterChoose} key={i} />
+                    <Letter letter={letter} onChoose={onLetterChoose} key={''+playCount+i}/>
                 )}
             </div>
         </div>
