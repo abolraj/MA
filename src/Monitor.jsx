c1331602c5acd2997302cf2defb0975696f4f40f
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-05 12:03:43 +0330

    fix - Remove width

--- src/Monitor.jsx
+++ src/Monitor.jsx
@@ -1,6 +1,6 @@
 function Monitor({text}) {
     return (
-        <div className="monitor text-lg flex items-center justify-start text-left bg-base-100 bordered border-2 border-info rounded-md p-2 px-4 w-72 h-10">
+        <div className="monitor text-lg flex items-center justify-start text-left bg-base-100 bordered border-2 border-info rounded-md p-2 px-4 h-10">
             <p>
                 {text}
             </p>
