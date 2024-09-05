b671805d5d0f357ac98ccabe7febc856a157c972
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-05 13:25:35 +0330

    feat - Add TheEnd component

---
+++ src/TheEnd.jsx
@@ -0,0 +1,29 @@
+function TheEnd() {
+    return (
+        <div className="fixed top-0 left-0 w-full flex text-3xl h-full justify-center items-center">
+            <div className="flex justify-center">
+
+                <p className="flex flex-col items-center text-[#EC407A] justify-start">
+                    <b className="text-9xl">M</b>
+                    <b>O</b>
+                    <b>B</b>
+                    <b>I</b>
+                    <b>N</b>
+                    <b>A</b>
+                </p>
+                <p className="flex flex-col items-center text-[#29B6F6] justify-start">
+                    <b className="text-9xl">A</b>
+                    <b>B</b>
+                    <b>O</b>
+                    <b>L</b>
+                    <b>F</b>
+                    <b>A</b>
+                    <b>Z</b>
+                    <b>L</b>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default TheEnd
