99c50cb613b34958142524507c650694e6c4e47d
abolraj <abolfazlrajaeenasab@gmail.com>
2024-09-05 09:50:22 +0330

    boot - Boot App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Guess from './Guess'
+
+const words = [
+  'hello',
+  'car',
+  'mobina',
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-
-      </div>
-      <p className="read-the-docs text-info">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main class="container-fluid flex flex-col items-center">
+      <Guess words={words}/>
+    </main>
   )
 }
 
